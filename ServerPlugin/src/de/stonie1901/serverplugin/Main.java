78821622afa6794137ca9a43b6e73b4aa27c19b2
Jan-Erik <stonie1901@gmail.com>
2015-02-19 12:57:27 +0100

    Added /teamspeak Command

--- ServerPlugin/src/de/stonie1901/serverplugin/Main.java
+++ ServerPlugin/src/de/stonie1901/serverplugin/Main.java
@@ -1,18 +1,29 @@
 package de.stonie1901.serverplugin;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.stonie1901.serverplugin.commands.teamspeakCommand;
+
 public class Main extends JavaPlugin {
 	
 	@Override
 	public void onDisable()
-	{	
+	{
+		getLogger().info("Sciencecraft Plugin wurde deaktiviert!");
 	}
 	
 	
 	@Override
 	public void onEnable()
 	{
-		plugin = this
+		getLogger().info("Sciencecraft Plugin wurde aktiviert!");
+		getCommands();
+		PluginManager pm = getServer().getPluginManager();
 	}
 	
-
+	public void getCommands()
+	{
+		this.getCommand("teamspeak").setExecutor(new teamspeakCommand(this));
+		
+	}
 }
---
+++ ServerPlugin/src/de/stonie1901/serverplugin/commands/teamspeakCommand.java
@@ -0,0 +1,14 @@
+package de.stonie1901.serverplugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class teamspeakCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		return false;
+	}
+
+}
