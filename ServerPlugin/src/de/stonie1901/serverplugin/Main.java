d73e71eebe136533b22a36bb007f71464dece219
Jan-Erik <stonie1901@gmail.com>
2015-02-19 16:27:42 +0100

    Added Metrics Support but not finished now

--- ServerPlugin/src/de/stonie1901/serverplugin/Main.java
+++ ServerPlugin/src/de/stonie1901/serverplugin/Main.java
@@ -1,10 +1,17 @@
 package de.stonie1901.serverplugin;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.io.IOException;
 
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.spigotmc.Metrics;
+
+
+import de.stonie1901.serverplugin.commands.teamspeakCommand;
 import de.stonie1901.serverplugin.commands.teamspeakCommand;
 
 public class Main extends JavaPlugin {
+	private static Main plugin;
 	
 	@Override
 	public void onDisable()
@@ -19,11 +26,17 @@ public class Main extends JavaPlugin {
 		getLogger().info("Sciencecraft Plugin wurde aktiviert!");
 		getCommands();
 		PluginManager pm = getServer().getPluginManager();
+		try {
+	        Metrics metrics = new Metrics();
+	        metrics.start();
+	    } catch (IOException e) {
+	        // Failed to submit the stats :-(
+	    }
 	}
 	
 	public void getCommands()
 	{
-		this.getCommand("teamspeak").setExecutor(new teamspeakCommand(this));
+		this.getCommand("teamspeak").setExecutor(new teamspeakCommand());
 		
 	}
 }
--- ServerPlugin/src/de/stonie1901/serverplugin/commands/teamspeakCommand.java
+++ ServerPlugin/src/de/stonie1901/serverplugin/commands/teamspeakCommand.java
@@ -1,13 +1,34 @@
 package de.stonie1901.serverplugin.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.stonie1901.serverplugin.Main;
 
 public class teamspeakCommand implements CommandExecutor {
+	public Main plugin;
 
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		
+		if (cmd.getName().equalsIgnoreCase("kits")) {
+		      if ((cs instanceof Player))
+		      {
+		        Player player = (Player)cs;
+		        if (player.hasPermission("EasyPvpKits.Kits"))
+		        {
+		        	player.sendMessage(ChatColor.BLACK + "test");
+		        	return true;
+		        } else
+		        {
+		        	player.sendMessage(ChatColor.RED + "You do not have permission to excute this command.");
+		            return true;
+		        }
+		      }
+		}
 		return false;
 	}
 
