b8ce0a135b98d80beb911e398623ce7a0e3f082d
Jan-Erik <stonie1901@gmail.com>
2015-02-19 12:46:46 +0100

    First

---
+++ ServerPlugin/src/de/stonie1901/serverplugin/Main.java
@@ -0,0 +1,18 @@
+package de.stonie1901.serverplugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onDisable()
+	{	
+	}
+	
+	
+	@Override
+	public void onEnable()
+	{
+		plugin = this
+	}
+	
+
+}
